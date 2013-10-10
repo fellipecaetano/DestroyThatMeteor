4553c46e17a404c8fb7d2e40feb532a9bfa5c016
Fellipe Caetano <fellipe.caetano4@gmail.com>
2013-10-10 14:12:13 -0300

    Initial Commit

---
+++ DestroyTheMeteors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DestroyTheMeteors
+//
+//  Created by Fellipe Caetano on 10/10/13.
+//  Copyright (c) 2013 Dextra Sistemas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
